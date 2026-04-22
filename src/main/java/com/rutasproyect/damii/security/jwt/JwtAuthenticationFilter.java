fc0945a9226d23b5ce030f6c27bbc77d588502d2
sebassa-hub <sebasnaruto32@gmail.com>
2026-04-22 12:42:45 -0500

    error de jwt authentication

--- src/main/java/com/rutasproyect/damii/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/rutasproyect/damii/security/jwt/JwtAuthenticationFilter.java
@@ -4,7 +4,6 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -29,9 +28,9 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(
-            @NonNull HttpServletRequest request,
-            @NonNull HttpServletResponse response,
-            @NonNull FilterChain filterChain) throws ServletException, IOException {
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain) throws ServletException, IOException {
 
         // 1. Extraer la cabecera Authorization
         final String authHeader = request.getHeader("Authorization");
